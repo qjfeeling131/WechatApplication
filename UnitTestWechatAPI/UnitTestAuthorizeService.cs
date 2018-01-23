e0aa4924de95418d0b604940a2f6a2ec826f0fac
pari Zhong <qjfeeling8994@sina.com>
2018-01-23 20:54:53 +0800

    optimize the unit test

---
+++ UnitTestWechatAPI/UnitTestAuthorizeService.cs
@@ -0,0 +1,23 @@
+using System;
+using Abp;
+using Abp.Dependency;
+using Abp.DoNetCore;
+using Xunit;
+
+namespace UnitTestWechatAPI
+{
+    public class UnitTestAuthorizeService
+    {
+        private readonly AbpBootstrapper _AbpBootStrapper = AbpBootstrapper.Create<AbpDoNetCoreModule>(IocManager.Instance);
+        public UnitTestAuthorizeService(){
+            _AbpBootStrapper.Initialize();
+            _AbpBootStrapper.IocManager.BuildComponent();
+        }
+
+        [Fact]
+        public void AuthorizationUser_ShouldGetTokenSuccessfully()
+        {
+
+        }
+    }
+}
