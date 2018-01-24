6dea15965626da71c9c2fdad535ada0e9ad1cadc
qjfeeling131 <qjfeeling8994@sina.com>
2018-01-24 10:49:05 +0800

    Optimize the directory structure of project

---
+++ test/UnitTestWechatServices/UnitTestAuthorizeService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Abp;
+using Abp.Dependency;
+using Abp.DoNetCore;
+using Xunit;
+
+namespace UnitTestWechatServices
+{
+    public class UnitTestAuthorizeService
+    {
+        private readonly AbpBootstrapper _AbpBootStrapper = AbpBootstrapper.Create<AbpDoNetCoreModule>(IocManager.Instance);
+
+        public UnitTestAuthorizeService()
+        {
+
+            _AbpBootStrapper.Initialize();
+
+            _AbpBootStrapper.IocManager.BuildComponent();
+
+        }
+        [Fact]
+        public void AuthorizationUser_ShouldGetTokenSuccessfully()
+
+        {
+        }
+
+    }
+}
